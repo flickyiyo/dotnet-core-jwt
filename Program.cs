4e5156f0af7c45c724959aec963a439b16961b38
Edgar <edgar21_9@hotmail.com>
2021-12-16 01:52:15 -0600

    Added header for auth on swagger

--- Program.cs
+++ Program.cs
@@ -1,5 +1,10 @@
 using Microsoft.EntityFrameworkCore;
 using courses_platform.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Filters;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,12 +13,31 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options => {
+    options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme{
+        Description = "Standard Authorization header sing the Bearer Scheme",
+        In = ParameterLocation.Header,
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey
+    });
+    options.OperationFilter<SecurityRequirementsOperationFilter>();
+});
+
 var serverVersion = new MySqlServerVersion(new Version(10, 4));
 builder.Services.AddDbContext<CoursesDbContext>(options => options.UseMySql(
-                serverVersion: serverVersion,
-                connectionString: builder.Configuration.GetConnectionString("Default")
-            ));
+    serverVersion: serverVersion,
+    connectionString: builder.Configuration.GetConnectionString("Default")
+));
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options => {
+        options.TokenValidationParameters = new TokenValidationParameters{
+            ValidateIssuer =false,
+            ValidateAudience = false,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JwtSettings:SignKey"])),
+            ValidateIssuerSigningKey = true,
+        };
+    });
 builder.Services.AddMvc();
 // builder.Services.AddApiVersioning();
 
@@ -28,6 +52,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
